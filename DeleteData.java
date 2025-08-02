e1beb02a9db049d5cf2ae9a8491cf09fff3ceb8d
Aditya Tile <adityatile99@gmail.com>
2025-08-02 19:38:52 +0530

    learn how to delete and update data

---
+++ DeleteData.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteData {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306/jdbc";
+        String username = "root";
+        String password = "aditya";
+        String q = "DELETE FROM user WHERE id = 5";
+        try {
+            Class.forName( "com.mysql.jdbc.Driver" );
+            System.out.println("Class Loaded Successfully");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            Connection con = DriverManager.getConnection( url, username, password );
+            Statement stmt = con.createStatement();
+            int affectedRows = stmt.executeUpdate( q );
+            if(affectedRows > 0) {
+                System.out.println("Data deleted successfully " + affectedRows + " rows affected");
+            } else {
+                System.out.println("Data Deletion failed");
+            }
+            stmt.close();
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
